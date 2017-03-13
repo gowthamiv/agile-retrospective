177f064125ce3bbb91619a8e279dc2d37d25f470
gowthamiv <gowthami.525@gmail.com>
2017-03-13 17:51:32 +0530

    adding new files

---
+++ src/app/landing-page/landing-page.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular2/core';
+
+@Injectable()
+export class LandingPageService {
+
+    getComments(){
+
+    }
+
+    addComment(){
+
+    }
+
+    updateComment() {
+
+    }
+
+}
